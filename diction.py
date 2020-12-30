abf14bffb6f0679140255b5190a76c19e477c910
9boogie <jzero.youngs89@gmail.com>
2020-12-29 23:43:03 -0800

    Object

--- diction.py
+++ diction.py
@@ -2,3 +2,32 @@ cabinet = {3: 'Tei', 100: 'Jay'}
 
 print(cabinet[3])
 print(cabinet.get(100))
+
+#print(cabinet[5]) error message because 5 is not existed
+print(cabinet.get(5))
+print(cabinet.get(5, "Usable"))
+
+
+print(3 in cabinet)
+print(5 in cabinet)
+
+cabinet[3] = 'Max'
+cabinet[7] = 'Nina'
+
+print(cabinet)
+
+del cabinet[7]
+print(cabinet)
+
+# Print only key
+print(cabinet.keys())
+
+# Print only value
+print(cabinet.values())
+
+# Print key and value
+print(cabinet.items())
+
+# Remove object
+cabinet.clear()
+print(cabinet)
---
+++ tuple.py
@@ -0,0 +1,8 @@
+menu = ('sushi', 'Udon')
+print(menu[0])
+
+# menu.add('chicken) Error - because tuple can't add or edit
+ 
+(name, age, hobby) = ("Will", 30, "Shopping")
+
+print(name, age, hobby)
