8eeefee460ad13fd204495eb10909d86e83f96a5
9boogie <jzero.youngs89@gmail.com>
2021-01-07 01:07:12 -0800

    Value def

---
+++ basic_value_def.py
@@ -0,0 +1,9 @@
+# def profile(name, age, main_lang):
+#   print("Name: {0}, Age: {1}, Main Lang: {2}".format(name, age, main_lang))
+
+# profile("Jae", 30, "JavaScript")
+
+def profile(name, age = 17, main_lang="Python"):
+  print("Name: {0}, Age: {1}, Main Lang: {2}".format(name, age, main_lang))
+
+profile("Paul")
